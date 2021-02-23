78972c2756fdc7d7e271fdef9cca5a1d85c4eb06
Erick <egonzalez23179@columbushs.com>
2021-02-23 12:23:28 -0500

    finished

--- sandbox.js
+++ sandbox.js
@@ -1,9 +1,10 @@
-console.log(1);
-console.log(2);
+const request = new XMLHttpRequest();
 
-setTimeout(() => {
-  console.log(`callback function fired`);
-}, 2000);
-
-console.log(3);
-console.log(4);
+request.addEventListener(`readystatechange`, () => {
+  // console.log(`request`, request.readyState)
+  if(request.readyState === 4){
+    console.log(request.responseText)
+  }
+})
+request.open(`GET`, `https://jsonplaceholder.typicode.com/todos/`)
+request.send();
