21c7e27a89a5af970e4519a6679da9efbd7fcf78
Erick <egonzalez23179@columbushs.com>
2021-02-25 12:58:57 -0500

    finished

--- sandbox.js
+++ sandbox.js
@@ -1,29 +1,18 @@
 
 
 const getTodos = async () => {
-  const response = await fetch(`luigi.json`)
+  const response = await fetch(`luiggi.json`)
+
+  if(response.statys !== 200){
+    throw new Error(`cannot fetch the data`);
+  }
+
   const data = await response.json();
 
   return data;
 
 };
 
-console.log(1);
-console.log(2);
-
 getTodos()
-.then(data => console.log(`resolved: ${data}`));
-
-console.log(3);
-console.log(4);
-
-
-
-// fetch(`luigi.json`).then((response) => {
-//   console.log(`resolved ${response}`)
-//   return response.json();
-// }).then(data => {
-//   console.log(data)
-// }).catch((err) => {
-//   console.log(`rejected ${err}`)
-// });
+  .then(data => console.log(`resolved: ${data}`))
+  .catch(err => console.log(`rejected: ${err.message}`))
