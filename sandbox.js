df865108dc92b1553189b1c4455957b300435995
Erick <egonzalez23179@columbushs.com>
2021-02-25 12:42:46 -0500

    finished

--- sandbox.js
+++ sandbox.js
@@ -1,55 +1,10 @@
-const getTodos = (resource, callback) => {
+// fetch api
 
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
-  
-    request.addEventListener('readystatechange', () => {
-  
-      if(request.readyState === 4 && request.status === 200){
-        const data = JSON.parse(request.responseText);
-        resolve(data);
-      } else if (request.readyState === 4){
-        reject(`error getting resource`)
-      }
-  
-    });
-  
-  
-    request.open(`GET`, resource)
-    request.send();
-  })
-}
-
-getTodos(`luigi.json`).then(data => {
-  console.log(`promise 1 resolved: ${data}`)
-  return getTodos(`mario.json`)
+fetch(`luigi.json`).then((response) => {
+  console.log(`resolved ${response}`)
+  return response.json();
 }).then(data => {
-  console.log(`promise 2 resolved: ${data}`)
-  return getTodos(`shaun.json`)
-}).then(data => {
-  console.log(`promise 3 resolved: ${data}`)
+  console.log(data)
 }).catch((err) => {
-  console.log(`promise rejected: ${err}`)
-})
-
-
-// const getSomething = () => {
-  
-//   return new Promise( (resolve, reject) => {
-//      resolve(`some data`)
-//      reject(`some error`)
-//   });
-
-// };
-
-// getSomething().then((data) => {
-//   console.log(data);
-// }, (err) => {
-//   console.log(err)
-// });
-
-// getSomething().then(data => {
-//   console.log(data)
-// }).catch(err => {
-//   console.log(err)
-// })
+  console.log(`rejected ${err}`)
+});
